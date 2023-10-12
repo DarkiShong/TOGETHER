94d75643239e47d87cdf031a821e03f6f8f102b0
alex_p <alexander.titanysky@gmail.com>
2023-10-12 14:28:25 +0300

    generate_example

---
+++ generate.py
@@ -0,0 +1,19 @@
+import random
+
+
+def generate_example(n):
+    data = []
+    operation = ['+', '*', '/', '-']
+    for i in range(n):
+        a = random.choice(operation)
+        if a == '+' or a == '-':
+            b = str(random.randint(0, 1000)) + a + str(random.randint(0, 1000))
+            data.append(b)
+        else:
+            b = str(random.randint(0, 100)) + a + str(random.randint(0, 100))
+            data.append(b)
+
+    return data
+
+
+print(generate_example(5))
