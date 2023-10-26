ad4533a21d0894ce5b4f8783b851d8fc41261c9f
alex_p <alexander.titanysky@gmail.com>
2023-10-26 14:24:25 +0300

    negative nambers, debug

--- generate.py
+++ generate.py
@@ -11,15 +11,24 @@ def generate_example(n, d, num_of_numbs):
         if '*' not in operation:
             b = ''
             for j in range(num_of_numbs):
-                b = b + str(random.randint(0, 100))
+                a = random.randint(-100, 100)
+                if a < 0:
+                    a = '(' + str(a) + ')'
+                b = b + str(a)
                 if j != num_of_numbs - 1:
                     b += str(random.choice(operation))
             data.append(b)
         else:
             b = ''
             for j in range(num_of_numbs):
-                b = b + str(random.randint(1, 10))
-                if j != num_of_numbs:
+                a = random.randint(-10, 10)
+                while a != 0:
+                    a = random.randint(-10, 10)
+
+                if a < 0:
+                    a = '(' + str(a) + ')'
+                b = b + str(a)
+                if j != num_of_numbs - 1:
                     b += str(random.choice(operation))
             data.append(b)
 
