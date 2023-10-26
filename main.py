7f55d1df1b4de39e2f8dbda1b83ef372769a3210
alex_p <alexander.titanysky@gmail.com>
2023-10-26 13:29:53 +0300

    added levels for frontend

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from generate import generate_example
 
 
 name = input("Your name is:")
+d = 0
+while d != '1' and d != '2':
+    print("Enter difficulty level (1 or 2)")
+    d = input()
+
 print("How many tasks do you want to solve:")
 kol = int(input())
 
-c = generate_example(kol)
-for i in range (0, kol):
+c = generate_example(kol, d)
+for i in range(0, kol):
     print(c[i])
     a = int(input())
     if a == eval(c[i]):
